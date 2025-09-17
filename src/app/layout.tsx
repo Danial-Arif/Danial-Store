e7ce718f2120397dbd69b5a200ff09f24eb14cba
Danial Arif <166061013+Danial-Arif@users.noreply.github.com>
2025-09-17 11:14:28 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,28 @@
-import './globals.css';
+import './globals.css'
 import SessionProviderWrapper from '../providers/SessionProviderWrapper'
-import Head from 'next/head';
+import { Toaster } from 'react-hot-toast'
+
 export const metadata = {
   title: 'Danial Arif Store',
   icons: {
     icon: '/Danial.png', // must be in public/
   },
-};
+}
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body>
-        <SessionProviderWrapper>{children}</SessionProviderWrapper>
+        <SessionProviderWrapper>
+          {children}
+          {/* 🔥 Mount Toaster globally */}
+          <Toaster position="top-center" />
+        </SessionProviderWrapper>
       </body>
     </html>
-  );
+  )
 }
